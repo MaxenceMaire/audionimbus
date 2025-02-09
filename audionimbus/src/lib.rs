3b1a235aa8896b8146b183f09eed64e04f722b7c
Maxence Maire <maire.maxence@gmail.com>
2025-02-09 21:38:31 +0100

    reorder lib exports

--- audionimbus/src/lib.rs
+++ audionimbus/src/lib.rs
@@ -4,8 +4,14 @@ pub use audio_buffer::*;
 pub mod audio_settings;
 pub use audio_settings::*;
 
+pub mod callback;
+pub use callback::*;
+
 pub mod context;
 pub use context::*;
+
+pub mod device;
+pub use device::*;
 
 pub mod effect;
 pub use effect::*;
@@ -21,11 +27,11 @@ pub use geometry::*;
 pub mod hrtf;
 pub use hrtf::*;
 
-pub mod version;
-pub use version::*;
+pub mod model;
+pub use model::*;
 
-pub mod device;
-pub use device::*;
+pub mod probe;
+pub use probe::*;
 
 mod serialized_object;
 pub use serialized_object::SerializedObject;
@@ -33,11 +39,5 @@ pub use serialized_object::SerializedObject;
 pub mod simulation;
 pub use simulation::*;
 
-pub mod model;
-pub use model::*;
-
-pub mod probe;
-pub use probe::*;
-
-pub mod callback;
-pub use callback::*;
+pub mod version;
+pub use version::*;
