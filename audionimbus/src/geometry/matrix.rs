b4b067146cfdf20595dcfb1f45f2c725992aa81e
Maxence Maire <maire.maxence@gmail.com>
2026-01-15 15:06:47 +0100

    test Matrix construction

--- audionimbus/src/geometry/matrix.rs
+++ audionimbus/src/geometry/matrix.rs
@@ -1,5 +1,5 @@
 /// A ROWSxCOLS matrix of type T elements.
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Matrix<T, const ROWS: usize, const COLS: usize> {
     /// Matrix elements, in row-major order.
     pub elements: [[T; COLS]; ROWS],
@@ -53,5 +53,68 @@ impl From<&Matrix<f32, 4, 4>> for audionimbus_sys::IPLMatrix4x4 {
         audionimbus_sys::IPLMatrix4x4 {
             elements: matrix.elements,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_matrix3x3_identity() {
+        let m = Matrix::<f32, 3, 3>::IDENTITY;
+
+        #[rustfmt::skip]
+        assert_eq!(
+            m,
+            Matrix {
+                elements: [
+                    [1.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                    [0.0, 0.0, 1.0],
+                ]
+            }
+        );
+    }
+
+    #[test]
+    fn test_matrix4x4_identity() {
+        let m = Matrix::<f32, 4, 4>::IDENTITY;
+
+        #[rustfmt::skip]
+        assert_eq!(
+            m,
+            Matrix {
+                elements: [
+                    [1.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 0.0],
+                    [0.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 1.0],
+                ]
+            }
+        );
+    }
+
+    #[test]
+    fn test_matrix_new() {
+        let elements = [[1.0, 2.0], [3.0, 4.0]];
+        let m = Matrix::new(elements);
+
+        #[rustfmt::skip]
+        assert_eq!(
+            m,
+            Matrix {
+                elements: [
+                    [1.0, 2.0],
+                    [3.0, 4.0],
+                ]
+            }
+        );
+    }
+
+    #[test]
+    fn test_matrix4x4_default() {
+        let m = Matrix::<f32, 4, 4>::default();
+        assert_eq!(m, Matrix::<f32, 4, 4>::IDENTITY);
     }
 }
