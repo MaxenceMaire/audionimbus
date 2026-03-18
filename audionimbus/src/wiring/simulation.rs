e89e07d5649e123e5e2a8518899831841127011f
Maxence Maire <maire.maxence@gmail.com>
2026-03-18 14:16:10 +0100

    add pathing to simulation builder

--- audionimbus/src/wiring/simulation.rs
+++ audionimbus/src/wiring/simulation.rs
@@ -1,8 +1,9 @@
 use super::{
-    Allocate, DirectFrame, DirectStep, ReflectionsFrame, ReflectionsOutput, ReflectionsReverbFrame,
-    ReflectionsReverbOutput, ReflectionsReverbStep, ReflectionsStep, SimulationRunner,
+    Allocate, DirectFrame, DirectStep, PathingFrame, PathingStep, ReflectionsFrame,
+    ReflectionsOutput, ReflectionsReverbFrame, ReflectionsReverbOutput, ReflectionsReverbStep,
+    ReflectionsStep, SimulationRunner,
 };
-use crate::effect::{DirectEffectParams, ReflectionEffectType};
+use crate::effect::{DirectEffectParams, PathEffectParams, ReflectionEffectType};
 use crate::ray_tracing::RayTracer;
 use crate::simulation::{
     Direct, DirectCompatible, Pathing, PathingCompatible, ReflectionEffectCompatible, Reflections,
@@ -168,11 +169,40 @@ where
 
 impl<T, D, R, RE> Simulation<T, D, R, Pathing, RE>
 where
-    T: RayTracer,
+    T: 'static + RayTracer,
+    D: 'static + Send + Sync + Clone + Default + DirectCompatible<D> + SimulationFlagsProvider,
+    R: 'static + Send + Sync + Clone + Default + ReflectionsCompatible<R> + SimulationFlagsProvider,
+    RE: 'static + Send + Sync + Clone + Default + ReflectionEffectCompatible<R, RE>,
+    (): DirectCompatible<D> + ReflectionsCompatible<R>,
+    Vec<PathEffectParams>: Allocate<PathingFrame<D, R, Pathing, RE>>,
 {
-    pub fn spawn_pathing(&self) -> PathingSimulation {
-        // TODO: implement pathing simulation builder.
-        todo!()
+    /// Spawns a pathing simulation thread.
+    pub fn spawn_pathing(&self) -> PathingSimulation<D, R, RE> {
+        let input = Arc::new(ArcSwap::new(Arc::new(PathingFrame {
+            sources: self.initial_sources.clone(),
+            shared_inputs: Default::default(),
+        })));
+
+        let output = SharedSimulationOutput(Arc::new(ArcSwap::new(Arc::new(
+            Arc::new(Pool::new(1, Vec::default)).pull_owned(Vec::default),
+        ))));
+
+        let mut simulator_for_commit = self.simulator.clone();
+        let handle = SimulationRunner::new(
+            input.clone(),
+            output.0.clone(),
+            self.commit_needed.clone(),
+            move || simulator_for_commit.commit(),
+        )
+        .spawn(PathingStep {
+            simulator: self.simulator.clone(),
+        });
+
+        PathingSimulation {
+            handle,
+            input,
+            output,
+        }
     }
 }
 
@@ -216,8 +246,16 @@ where
 }
 
 /// Running pathing simulation thread.
-pub struct PathingSimulation {
+pub struct PathingSimulation<D, R, RE>
+where
+    RE: ReflectionEffectCompatible<R, RE>,
+{
+    /// Thread handle.
     pub handle: std::thread::JoinHandle<()>,
+    /// Shared input frame, updated each game frame.
+    pub input: Arc<ArcSwap<PathingFrame<D, R, Pathing, RE>>>,
+    /// Shared output, read by the audio thread.
+    pub output: SharedSimulationOutput<Vec<PathEffectParams>>,
 }
 
 /// A pair of source and simulation inputs.
